562c8e829f57ebd3b68d630a46629ca2517a6775
hariharan020703 <hariharan.pappannan@gwcdata.ai>
2026-01-08 19:51:39 +0530

    Adding All Files

---
+++ src/components/auth/SignUpForm.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import { EyeCloseIcon, EyeIcon } from "../../icons";
+import Label from "../form/Label";
+import Input from "../form/input/InputField";
+import logo from "../../assets/logo.jpg";
+
+export default function SignUpForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="flex flex-col flex-1 w-full overflow-y-auto lg:w-1/2 no-scrollbar">
+      <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
+        <div>
+          <div className="flex justify-center mb-6">
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-24 sm:w-28 md:w-32 lg:w-36 object-contain"
+            />
+          </div>
+          <div className="mb-5 sm:mb-8">
+            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+              Sign Up
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Enter your email and password to sign up!
+            </p>
+          </div>
+          <div>
+            <div className="relative py-3 sm:py-5">
+            </div>
+            <form>
+              <div className="space-y-5">
+                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                  {/* <!-- First Name --> */}
+                  <div className="sm:col-span-1">
+                    <Label>
+                      First Name<span className="text-error-500">*</span>
+                    </Label>
+                    <Input
+                      type="text"
+                      id="fname"
+                      name="fname"
+                      placeholder="Enter your first name"
+                    />
+                  </div>
+                  {/* <!-- Last Name --> */}
+                  <div className="sm:col-span-1">
+                    <Label>
+                      Last Name<span className="text-error-500">*</span>
+                    </Label>
+                    <Input
+                      type="text"
+                      id="lname"
+                      name="lname"
+                      placeholder="Enter your last name"
+                    />
+                  </div>
+                </div>
+                {/* <!-- Email --> */}
+                <div>
+                  <Label>
+                    Email<span className="text-error-500">*</span>
+                  </Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                {/* <!-- Password --> */}
+                <div>
+                  <Label>
+                    Password<span className="text-error-500">*</span>
+                  </Label>
+                  <div className="relative">
+                    <Input
+                      placeholder="Enter your password"
+                      type={showPassword ? "text" : "password"}
+                    />
+                    <span
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
+                    >
+                      {showPassword ? (
+                        <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                      ) : (
+                        <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                      )}
+                    </span>
+                  </div>
+                </div>
+
+                {/* <!-- Button --> */}
+                <div>
+                  <button className="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-[#83261D] shadow-theme-xs hover:bg-[#83261D]">
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <div className="mt-5">
+              <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
+                Already have an account? {""}
+                <Link
+                  to="/signin"
+                  className="text-[#83261D] hover:text-[#83261D] dark:text-[#83261D]"
+                >
+                  Sign In
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
+import AuthPageLayoutImg from "../../assets/AuthPageLayoutImg.jpg"
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+      <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
+        {children}
+        <div className="items-center hidden w-full h-full lg:w-1/2 dark:bg-white/5 lg:grid">
+          <div className="relative flex items-center justify-center z-1">
+            {/* <!-- ===== Common Grid Shape Start ===== --> */}
+            <div className="relative flex items-center justify-center w-full h-screen overflow-hidden">
+              <img
+                src={AuthPageLayoutImg}
+                alt="Auth Illustration"
+                className="w-full max-h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+        {/* <div className="fixed z-50 hidden bottom-6 right-6 sm:block">
+          <ThemeTogglerTwo />
+        </div> */}
+      </div>
+    </div>
+  );
+}
