c3c444a9d4f30adc4fbb081f3f041622018ce96b
hariharan020703 <hariharan.pappannan@gwcdata.ai>
2026-01-16 17:52:24 +0530

    Added Update and Delete for ArtTypes

--- src/api/artCategoryApi.ts
+++ src/api/artCategoryApi.ts
@@ -7,3 +7,15 @@ export const createCategoryApi = (data: FormData) =>
 
 export const deleteCategoryApi = (id: string) =>
   api.delete(`/categories/${id}`);
+
+export const updateArtTypeApi = (
+  categoryId: string,
+  artTypeId: string,
+  data: FormData
+) => api.put(`/categories/${categoryId}/arttype/${artTypeId}`, data);
+
+export const deleteArtTypeApi = (categoryId: string, artTypeId: string) =>
+  api.delete(`/categories/${categoryId}/arttype/${artTypeId}`);
+
+export const addArtTypeApi = (categoryId: string, data: FormData) =>
+  api.post(`/categories/${categoryId}/arttype`, data);
